0d61dfe82cc718752dbb925476a3502343cc9597
yaroslav-kulyk <kulyk.yaroslav@gmail.com>
2022-01-17 17:59:36 +0200

    done with avatar upload

--- model/user.js
+++ model/user.js
@@ -21,6 +21,10 @@ const userSchema = Schema(
       type: String,
       default: null,
     },
+    avatarURL: {
+      type: String,
+      default: "",
+    },
   },
   { versionKey: false, timestamps: true }
 );
